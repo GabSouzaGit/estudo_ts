fd7e9f4b8ff7b2bd3be82d74dd937c0904575250
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2023-12-01 16:41:23 -0300

    first

---
+++ functions/memeoize.ts
@@ -0,0 +1,28 @@
+type Fn = (...params: any) => any
+
+/**
+ * 
+ * @param fn {Fn} Uma função que recebe um numero indefinido de parametros e retorna qualquer dado.
+ * @returns Uma versão memoizada da função passada.
+ */
+
+function memoize(fn: Fn): Fn {
+    const cache : {
+        [key : string] : any
+    } = {}
+
+    return function(...params : any) {
+        const key = JSON.stringify(params);
+        if(key in cache){
+            return cache[key];
+        }
+
+        const result = fn(...params);
+        cache[key] = result;
+        return result;
+    }
+}
+
+export default memoize;
+
+
