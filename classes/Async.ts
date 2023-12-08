5f9cb449af5c1dd5f174e09e87717c653088330f
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2023-12-07 22:34:21 -0300

    :sparkles: Classe Async e fetching memoizado

---
+++ classes/Async.ts
@@ -0,0 +1,38 @@
+type CachedRequests = {
+    [key : string] : any
+}
+
+class Async extends Promise<PromiseConstructor>{
+    private static cachedRequests : CachedRequests = {};
+    constructor(callback : (resolve : any, reject : any) => (any)){
+        super(callback);
+    }
+
+    /**
+     * @description Realiza uma requisição para um serviço de forma memoizada, cacheando o resultado.
+     * @param {string} url URL da API ou serviço a ser consumido. 
+     * @param {object} options Configurações para o fetching.
+     * @returns {any} Resultado do serviço consumido.
+     */
+    public static async fetch(url : string, callback : any, options = {}){
+        if(url in this.cachedRequests){
+            return this.cachedRequests[url];
+        }
+
+        const request = await fetch(url, options);
+        const response = await request.json();
+
+        callback()
+        this.cachedRequests[url] = response;
+        return response;
+    }
+
+    /**
+     * @description Limpa o cache de requisições.
+     */
+    public static flushCache(){
+        this.cachedRequests = {};
+    }
+}
+
+export default Async;
