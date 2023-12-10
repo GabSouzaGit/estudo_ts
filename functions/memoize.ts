2e5552262dbb1c6b607add867e3b393446920af5
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2023-12-10 11:46:20 -0300

    :pencil:  Documentação da function memoize

--- functions/memoize.ts
+++ functions/memoize.ts
@@ -1,7 +1,6 @@
 type Fn = (...params: any) => any
 
 /**
- * 
  * @param fn {Fn} Uma função que recebe um numero indefinido de parametros e retorna qualquer dado.
  * @returns Uma versão memoizada da função informada.
  */
