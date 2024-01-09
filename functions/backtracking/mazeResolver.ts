3904f81c986ae6b01244fb08df4395c19dfff64e
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-01-09 13:54:27 -0300

    :construction: Resolvedor de labirinto em desenvolvimento

---
+++ functions/backtracking/mazeResolver.ts
@@ -0,0 +1,9 @@
+type Maze = number[][];
+type Local = {
+    x: number,
+    y: number
+}
+
+function mazeResolver(maze : Maze, start : Local, exit : Local){
+    const path : Local[] = [];
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,1 @@
-import combine from "./functions/combine";
 
-console.log(combine(4, 2));
