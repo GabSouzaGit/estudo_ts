90700d07c5f2c6b3caab5743f3e18275bedc75bb
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-01-09 15:05:36 -0300

    :sparkles: Solucionador de labirinto

--- functions/backtracking/mazeResolver.ts
+++ functions/backtracking/mazeResolver.ts
@@ -1,9 +1,57 @@
-type Maze = number[][];
+type Maze = (number | string)[][];
 type Local = {
     x: number,
     y: number
 }
 
-function mazeResolver(maze : Maze, start : Local, exit : Local){
-    const path : Local[] = [];
+const directions : Local[] = [
+    { x: 0, y: -1 },
+    { x: 0, y: 1 },
+    { x: 1, y: 0 },
+    { x: -1, y: 0 }
+]
+
+function mazeResolver(maze : Maze, start : Local, final : Local) : Maze{
+    function resolver(position : Local, exit : Local){
+        const path : Local[] = [];
+        const notInTheMaze = position.x < 0 || position.y < 0; 
+        const afterTheMaze = position.x >= maze[0].length || position.y >= maze.length;
+
+        if(notInTheMaze || afterTheMaze){
+            return false;
+        }
+
+        if(position.x == exit.x && position.y == exit.y){
+            maze[position.y][position.x] = 2;
+            return true;
+        }
+        
+        const openPath = maze[position.y][position.x] === 1;
+
+        if(openPath){
+            path.push(position);
+            maze[position.y][position.x] = 2;
+
+            for(let pos = 0; pos < directions.length; pos++){
+                const nextPosition = {
+                    x: position.x + directions[pos].x,
+                    y: position.y + directions[pos].y
+                }
+
+                if(resolver(nextPosition, exit)){
+                    return true
+                }
+            }
+            
+            path.pop();
+        }
+
+        return false
+    }
+
+    resolver(start, final)
+
+    return maze;
 }
+
+export default mazeResolver;
