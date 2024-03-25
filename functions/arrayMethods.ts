1a17d80a2cac8c8e253794b1b9082df43f422a07
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-03-25 16:32:04 -0300

    :sparkles: Filter function

--- functions/arrayMethods.ts
+++ functions/arrayMethods.ts
@@ -1,5 +1,6 @@
 type Mapper = (item : any, i : number, array : any[]) => any;
 type Reducer = (acc : any, curr : any) => any;
+type FilterFunction = (target : any, index? : number, array? : any[]) => boolean
 
 /**
  * @param {any[]} array Um array de n posições de qualquer tipo. 
@@ -24,7 +25,7 @@ export function map(array : any[], callback : Mapper){
  * 
  * @param {any[]} array Um array de n posições de qualquer tipo. 
  * @param {Reducer} callback Uma função que obtém a variavel acumuladora e o item atual. Retorna um novo valor para o acumulador.
- * @param init Valor inicial do acumulador, sendo por padrão o primeiro valor do array.
+ * @param {init} init Valor inicial do acumulador, sendo por padrão o primeiro valor do array.
  * @returns A versão reduzida do array.
  */
 
@@ -37,4 +38,35 @@ export function reduce(array : any[], callback : Reducer, init = array[0]){
     }
 
     return acc;
+}
+
+/**
+ * 
+ * @param {any[]} array Um array de n posições de qualquer tipo. 
+ * @param {FilterFunction} callback Função que retorna um booleano como resultado do filtro
+ * @returns {any[]} Versão filtrada do array.
+ */
+export function filter(array : any[], callback : FilterFunction){
+    const filtered : any[] = [];
+    for(let x = 0; x < array.length; x++){
+        if(callback(array[x], x, array)){
+            filtered.push(array[x])
+        }
+    }
+
+    return filtered;
+}
+
+/**
+ * 
+ * @param {number[]} array Um array numérico de n posições.
+ * @returns Um novo array com apenas números pares.
+ */
+export function getOnlyPairs(array : number[]){
+    const pairArray : number[] = [];
+    for(let x = 0; x < array.length; x++){
+        if(array[x] % 2 === 0) pairArray.push(array[x]);
+    }
+
+    return pairArray;
 }
