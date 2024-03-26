a0e5a411cdad3424cedf07d51ea7206c10e0f2d0
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-03-26 14:02:54 -0300

    :sparkles: Simulador de API

---
+++ functions/fetching/APISimulation.ts
@@ -0,0 +1,39 @@
+import possibility from "../math/possibility"
+
+const resolveStruct = [
+    {
+        name: 'Usuário A',
+        born: 1996
+    },
+    {
+        name: 'Usuário B',
+        born: 2005
+    },
+    {
+        name: 'Usuário C',
+        born: 1985
+    }
+]
+
+/**
+ * 
+ * @param {number | [number, number]} delay Tempo de demora para o retorno da Promise.
+ * @param {number} errorChance Valor inteiro entre 0 e 1 que represente o percentual de geração de exceções na função.
+ * @returns Uma Promise contendo ou os dados de retorno (resolve) ou um erro (reject).
+ */
+
+export default function APISimulation(delay : number | [number, number], errorChance : number){
+    const getPromise = (time : number) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                if(possibility(errorChance, 100)) reject(new Error('Falha na obtenção dos dados.'));
+                resolve(resolveStruct);
+            }, time);
+        });
+    }
+
+    if(typeof delay === 'number') return getPromise(delay);
+    
+    const roundedDelay = Math.floor(Math.random() * delay[1] + delay[0]);
+    return getPromise(roundedDelay);
+}
--- functions/math/possibility.ts
+++ functions/math/possibility.ts
@@ -1,8 +1,16 @@
-function possibility(percentual : number, total : number){
+/**
+ * 
+ * @param {number} percentual Numero entre 1 e 0 que represente a chance de um retorno verdadeiro.
+ * @param {number} total Numero inteiro de casos totais.
+ * @returns {boolean} Valor verdadeiro ocorrer tal possibilidade, senão, falso.
+ */
+
+function possibility(percentual : number, total : number = 1){
     const random = (n : number) => {
         return Math.floor(Math.random() * n)
     }
 
+    if(percentual > 1 || percentual < 0) throw new Error('O percentual precisa ser um numero entre 1 e 0');
     const chance = percentual * total;
     return random(total) <= chance;
 }
