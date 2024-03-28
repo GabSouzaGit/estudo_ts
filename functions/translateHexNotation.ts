3c6675cbe8794e66a8200e04f925f5d8b4f59d98
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-03-27 22:22:24 -0300

    :sparkles: Conversor hexadecimal

---
+++ functions/translateHexNotation.ts
@@ -0,0 +1,15 @@
+type HexAdvancedPartition = `${1|2|3|4|5|6|7}${0|1|2|3|4|5|6|7|8|9|'A'|'B'|'C'|'D'|'E'|'F'}`
+type HexLetterPartition = 'A' | 'B' | 'C' | 'D' | 'E' | 'F';
+type HexNumberPartition = `${0|1|2|3|4|5|6|7|8|9}`;
+type HexValue = HexNumberPartition | HexLetterPartition | HexAdvancedPartition;
+type Hexadecimal = HexValue[]
+
+export default function translateHexNotation(hex : Hexadecimal){
+    let translated : string ='';
+    for(let i = 0; i < hex.length; i++){
+        const dec = parseInt(hex[i], 16);
+        translated += String.fromCharCode(dec);
+    }
+
+    return translated;
+}
