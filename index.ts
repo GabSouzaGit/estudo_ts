ff55bf9000a7a206038c50672342426cb9041c30
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-05-18 12:56:15 -0300

    Limpar index.ts

--- index.ts
+++
@@ -1,9 +0,0 @@
-import PROCV from "./functions/excel/PROCV";
-
-const table = [
-    ['Nome',     'Setor',     'Idade',  'Trabalhando'],
-    ['Gabriel',  'Produção',  '19',     true],
-    ['Jorginho', 'RH',        '20',     false]
-]
-
-console.log(PROCV('B2', table, 1))
