25ec8f6324309d922276442e4c0b6abdd408648d
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-05-24 22:53:29 -0300

    :sparkles: Array prototype modificado

--- hacked_prototypes/Array.ts
+++ hacked_prototypes/Array.ts
@@ -1,6 +1,8 @@
 declare global {
     interface Array<T> {
-        getOnlyPairs() : number[]
+        getOnlyPairs() : number[],
+        stoppableForEach(callback : (x : T, die : Function, index? : number, array? : T[]) => void) : void,
+        fillWithCurrying(first : number) : (last : number) => (value : any) => boolean;  
     }
 }
 
@@ -15,6 +17,39 @@ Array.prototype.getOnlyPairs = function(){
     }
 
     return pairArray;
+}
+
+Array.prototype.stoppableForEach = function(callback : (x : any, die : Function, index? : number, array? : any[]) => void){
+    const die = () => {
+        throw new Error('DIE_FOREACH_EXECUTION');
+    }
+
+    try {
+        this.forEach((e : any, i : number) => {
+            callback(e, die, i, this);
+        })
+    }catch(exception : any){
+        if(exception.message === 'DIE_FOREACH_EXECUTION') return;
+        throw exception;
+    }
+}
+
+Array.prototype.fillWithCurrying = function(first : number){
+    return (last : number) => {
+        return (value : any) => {
+            if(first < 0 || first >= this.length
+            || last < 0 || last >= this.length
+            || first >= last){
+                return false;
+            }
+
+            for(let i = first; i <= last; i++){
+                this[i] = value;
+            }
+
+            return true;
+        }
+    }
 }
 
 export {};
--- index.ts
+++
@@ -1,3 +0,0 @@
-import './hacked_prototypes/Array';
-
-console.log([1,2,3,4,5,6].getOnlyPairs())
