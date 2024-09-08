176f3cb8403ef01a0d06272b22809b9fb44c02e6
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-09-08 19:50:53 -0300

    :sparkles: Árvore binária

---
+++ classes/data_structures/non-linear/BinaryTree.ts
@@ -0,0 +1,49 @@
+type BinaryTreeType = {
+    value : number,
+    left : BinaryTreeType | null,
+    right : BinaryTreeType | null
+}
+
+function TreeNode(value : number){
+    this.value = value
+    this.left = null,
+    this.right = null
+}
+
+export default class BinaryTree{
+    private head : BinaryTreeType;
+    public nodeQtd : number = 0;
+    
+    constructor(value : number){
+        this.head = new TreeNode(value);
+        this.nodeQtd = 1;
+    }
+
+    public append(value : number){
+        let currentRef = this.head;
+
+        while(true){
+            if(currentRef.value >= value){
+                if(currentRef.left === null){
+                    currentRef.left = new TreeNode(value);
+                    return;
+                }
+
+                currentRef = currentRef.left;
+                continue;
+            }else{
+                if(currentRef.right === null){
+                    currentRef.right = new TreeNode(value);
+                    return;
+                }
+
+                currentRef = currentRef.right;
+                continue;
+            }
+        }
+    }
+
+    public getRepresentation(){
+        return JSON.stringify(this.head, null, 2);
+    }
+}
