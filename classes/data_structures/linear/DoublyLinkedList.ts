d4e38d7b1b2ca965ec9a35d2afcbb502432aa4c3
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2024-09-28 16:12:06 -0300

    :construction: Construindo lista duplamente linkada

--- classes/data_structures/linear/DoublyLinkedList.ts
+++ classes/data_structures/linear/DoublyLinkedList.ts
@@ -1,1 +1,39 @@
-class DoublyLinkedList<T>{/* ... */}
+type DoublyLinkedListType<T> = {
+    previous : DoublyLinkedListType<T>| null,
+    value : T,
+    next : DoublyLinkedListType<T> | null
+}
+
+export default class DoublyLinkedList<T>{
+    private struct : DoublyLinkedListType<T>
+
+    constructor(first : T){
+        this.struct = {
+            previous: null,
+            value: first,
+            next: null
+        }
+    }
+
+    public appendNext(value : T){
+        let current = this.struct;
+        
+        while(true){
+            if(current.next === null){
+                current = {
+                    previous: { ...current },
+                    value,
+                    next: null
+                }
+
+                this.struct = current
+
+                return;
+            }
+        }
+    }
+
+    public getDataStructure(){
+        return JSON.stringify(this.struct, null, 2)
+    }
+}
