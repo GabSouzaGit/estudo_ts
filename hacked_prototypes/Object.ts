04a46307e52577f3d65b97fb7a793bba5ad01c5f
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2025-09-12 12:49:52 -0300

    :sparkles: Mapeador de Objetos

--- hacked_prototypes/Object.ts
+++ hacked_prototypes/Object.ts
@@ -1,5 +1,24 @@
+type ObjectMapCallback = (value : any, key : string, obj : object) => any
+
 declare global {
-    interface Object {}
+    interface Object {
+        map(obj : object, callback : ObjectMapCallback) : void;
+    }
+}
+
+Object.prototype.map = (obj : object, callback : ObjectMapCallback) => {
+    const keys = Object.keys(obj);
+    const copy = { ...obj }
+
+    for(let i = 0; i < keys.length; i++){
+        copy[keys[i]] = callback(
+            copy[keys[i]],
+            keys[i],
+            copy
+        )
+    }
+
+    return copy;
 }
 
 export {};
