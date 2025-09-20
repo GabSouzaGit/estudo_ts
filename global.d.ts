bc84934cb417107c31d03bd78822611e6d88f5c0
GabSouzaGit <103861777+GabSouzaGit@users.noreply.github.com>
2025-09-20 18:20:45 -0300

    :sparkles: Mensagem de aviso na mesclagem

--- global.d.ts
+++ global.d.ts
@@ -1,1 +1,8 @@
 // Declaração global de tipos
+
+type Color = 'red' |
+'green' |
+'yellow' |
+'blue' |
+'purple' |
+'cyan';
--- hacked_prototypes/Object.ts
+++ hacked_prototypes/Object.ts
@@ -1,3 +1,5 @@
+import paintText from "../utils/paintText";
+
 type ObjectMapCallback = (value : any, key : string, obj : object) => any
 
 declare global {
@@ -23,7 +25,10 @@ Object.prototype.map = (obj : object, callback : ObjectMapCallback) => {
 }
 
 Object.prototype.merge = (...objects : object[]) => {
+    const passedKeys = {};
+
     let merged = {};
+    let showWarnMessage = false;
 
     for(let i = 0; i < objects.length; i++){
         const obj = objects[i];
@@ -32,10 +37,19 @@ Object.prototype.merge = (...objects : object[]) => {
 
         for(let j = 0; j < keysToMerge.length; j++){
             const key = keysToMerge[j];
+
+            if(key in passedKeys) {
+                showWarnMessage = true;
+            }else {
+                passedKeys[key] = 1;
+            }
+
             merged[key] = obj[key];
         }
     }
     
+    if(showWarnMessage) console.log(`${paintText("ATENÇÃO:", 'red')} Alguns objetos da sua mesclagem ${paintText("possuem chaves iguais", 'yellow')}.\nO valor final dessa chave na mesclagem será igual ao valor da chave do ${paintText("ultimo objeto contendo ela", 'yellow')}.\n`)
+
     return merged;
 }
 
